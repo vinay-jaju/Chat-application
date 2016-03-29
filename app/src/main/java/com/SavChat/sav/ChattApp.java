1c5948b6cc770a91c44b4502a3933817380bb129
vinay-jaju <vinaykjaju@gmail.com>
2016-03-29 22:43:48 +0530

    Chat application

---
+++ app/src/main/java/com/SavChat/sav/ChattApp.java
@@ -0,0 +1,26 @@
+package com.SavChat.sav;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+/**
+ * The Class ChattApp is the Main Application class of this app. The onCreate
+ * method of this class initializes the Parse.
+ */
+public class ChattApp extends Application
+{
+
+	/* (non-Javadoc)
+	 * @see android.app.Application#onCreate()
+	 */
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
+
+		Parse.initialize(this, "JKIhwEFZJbny0BiLKRwfDJpKaFAhSNDU0zkeJnf6",
+				"xN1BhbmeL6iGxI0oo1NU8r025KlHStEZ5hl2dkYA");
+
+	}
+}
